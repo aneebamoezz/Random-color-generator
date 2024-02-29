80cca7ed2bcc756235450063b7c468c29cea3688
aneebamoezz <anibakaleem@gmail.com>
2024-03-01 03:36:27 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RandomColor from './components/RandomColor';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomColor />
+    </div>
+  );
+}
+
+export default App;
